eea620ea036b5b64dfd65cec1aae9c3c149a694f
JohnAVu <johnavu.it@gmail.com>
2018-10-21 23:11:27 +0700

    danh sach cau hoi

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Route,
-  } from 'react-router-dom';
+} from 'react-router-dom';
 import Home from '../views/Home'
+import ListQuestions from '../views/ListQuestions'
 
 const AppRouter = () =>
-  <Router >
-    <Route exact path='/' component={Home}/>
-  </Router>
+    <Router>
+        <div>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/danh-sach-cau-hoi' component={ListQuestions}/>
+        </div>
+
+    </Router>
 
 export default AppRouter;
