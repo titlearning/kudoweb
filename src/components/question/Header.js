dec6b10797e2f488cc746a3f9ae636df6dccf3ab
minhvd2 <minhvd2@topica.edu.vn>
2018-10-30 02:57:13 +0700

    merge

--- src/components/question/Header.js
+++ src/components/question/Header.js
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import Typography from '@material-ui/core/Typography';
-import {} from '../../'
+import { database } from '../../config/firebase'
 
 class Header extends Component {
     constructor(props) {
